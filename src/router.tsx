54bea57082e4a37e67cfd285dda5bb97091269bb
wooleejaan <wooleejaan@gmail.com>
2023-10-04 21:49:47 +0900

    fix: 라우터 파일 이름 변경 (main => router)

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,16 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import ConfettiPage from "./pages/ConfettiPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/confetti",
+    element: <ConfettiPage />,
   },
 ]);
 
